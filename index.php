ab19a1560e553f8d62d63ca57fbe99c1ed98f6b2
Maysoon Doucanse <maysoondoucanse@gmail.com>
2019-01-13 14:51:58 +0100

    la pagination

--- index.php
+++ index.php
@@ -32,28 +32,66 @@ session_start();
             </div>
             <div class="row">
                 <div class="col-sm-10">
+                    
             	<?php 
-            		include("includes/connexion.inc.php");
+            include("includes/connexion.inc.php");
+                    //déclaration de la page limite
+                        $nbPageLimit = 5;
+                    /**
+                    *   Count le nombre d'occurrence de la table messages 
+                    */
+                        $nbreMessage = $pdo->query("SELECT COUNT(id) as nombre FROM messages");
+                        $nbreMessage = $nbreMessage->fetch()['nombre'];
+            		
+                        //nombre de page
+                        $nbpage = ceil($nbreMessage / $nbPageLimit);
+                        
+                        // la page courrant 
+                        $pageCourant = (isset($_GET['p']) && $_GET['p'] > 0 && $_GET['p'] <= $nbpage) ? $_GET['p'] : 1;
 
+                        // page d'acceuil
+                        $debut = ($pageCourant - 1) * $nbPageLimit;
+
+                        //page antérieur
+                        $pageAnt = $pageCourant - 1;
+
+                        // page suivante
+                        $pageSuiv = $pageCourant + 1;
+                        
             		//recuperation et affichage du contenue
-            		$query="SELECT * FROM messages";
+            		$query="SELECT * FROM messages ORDER BY date_ajout DESC LIMIT $debut, $nbPageLimit";
             		$result = $pdo->prepare($query);
             		$result->execute();
             		while($row = $result->fetch())
                         {
             			$message = $row['contenu'];
                         $id = $row['id'];
-            			//echo "<blockquote><p>".$row['contenu']."</p>";
+                        $date = $row['date_ajout'];
                     	//echo gmdate("Y-m-d H:i:s", $row['date_ajout']);
         				 ?>
-                        <blockquote class="center">
+                        <div class="col-md-12">
+                        <blockquote>
                             <p><?php echo $message; ?></p>
         				        <a href="modification.php?id=<?= $id ?>" class="btn btn-info"> Modifier</a>
  							    <a href="suppression.php?id=<?= $id ?>" class="btn btn-danger">Supprimer</a>
-                                <a href="#" class="btn btn-primary">Vote</a>
+                                <a href="#" data-id="<?= $data['id'] ?>" class="btn btn-primary buttonVote">Vote</a><small class="text-muted nbreVote"> Nombre de vote : <?php echo "12" ?><p><?php echo $date; ?></p></small>
                     </blockquote>
+                            </div>
                 <?php  } ?>
             </div>
+                
+                <!--Pagination -->
+                <nav>
+                    <ul class="pagination pagination-lg">
+                        <li class="<?= ($pageAnt <= 1)? 'disabled' : '' ?>"><a href="?p=<?= $pageAnt ?>">&laquo;</a></li>
+                        <?php 
+                            for ($i = 1; $i <= $nbpage; $i++) {
+                            ?><li class="<?= ($i == $pageCourant)? 'disabled' : '' ?>"><a href="?p=<?= $i ?>"><?= $i ?></a></li><?php
+                            }
+                        ?>
+                        <li class="<?= ($pageSuiv >= $nbpage) ? 'disabled' : '' ?>"><a href="?p=<?= $pageSuiv ?>">&raquo;</a></li>
+                    </ul>
+                </nav>
         </div>
     
     </section>
@@ -61,13 +99,16 @@ session_start();
     <?php include("includes/bas.inc.php"); ?>
 
      <script type="text/javascript">
-            $.ajax({
+         $(".buttonVote").click(function(){
+                console.log( $.ajax({
                 url: 'vote.php'
                 method: 'POST'
-                data: {id: }
-                   }).done(function(retour){
-                    nombre de vote.html()
+                data: {id: $(this).attr('data-id')}
+                   }).done(function(data){
+                    $(".nbreVote").html();
             });
+            );
             });
+           
     
     </script>
--- vote.php
+++ vote.php
@@ -1,16 +1,28 @@
 <?php 
 include("includes/connexion.inc.php");
+//recuperer l'adresse ip du visiteur 
 $ip = $_SERVER['REMOTE_ADDR'];
-$id= $_POST['id'];
+// je recupere l'id que j'ai mis dans data-id
+$id= $_GET['id'];
+// la req qui permet de recupere l'id du msg
 $prep=$pdo->prepare("SELECT * FROM messages Where id= $id");
 $prep->bindvalue(":id", $id);
 $prep->execute();
-//je recupère la donnée
+//je recupère le resultat ensuite faire une comparaison 
 while ($recup=$prep->fetch()){
     $derniereip= recup['derniere_ip'];
     $vote= recup['vote'];
+    if ($ip!=$derniereip){
+    $reqt=("UPDATE messages SET derniere_ip where id= $id");
+    $prep=$pdo->prepare($reqt); 
+    $prep->execute($dernierip, $id);
+}else {
+    echo" vous avez déjà voté"; 
 }
-    
+  
+}
+  
 
 
 ?>
+
